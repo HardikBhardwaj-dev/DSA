6dcc810c80ec52aa0fcb22b02a55b37c2b95b53e
Hardik Bhardwaj <thehardikbhardwaj@gmail.com>
2026-09-01 10:34:56 +0530

    bubble and selection sort

---
+++ gfg_questions/selection_sort.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    void selectionSort(vector<int> &arr) {
+        // code here
+        for(int i=0;i<arr.size()-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.size();j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            swap(arr[i],arr[min]);
+        }
+    }
+};
